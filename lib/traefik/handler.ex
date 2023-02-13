51a415efab391cc02bd26178eb19eb121d8a8f5f
technosoul <rockdrigo.mtz@gmail.com>
2023-02-13 14:33:08 -0600

    def handdler

---
+++ lib/traefik/handler.ex
@@ -0,0 +1,16 @@
+defmodule Traefik.Handler do
+  def handle(request) do
+    request
+    |> parse()
+    |> route()
+    |> format_response()
+  end
+end
+
+request = """
+GET /hello HTTP/1.1
+Accept: */*
+Connection: keep-alive
+User-Agent: telnet
+
+"""
