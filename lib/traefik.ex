b55a4d2c631d66d62f6c96aceee2c84cf0681970
technosoul <rockdrigo.mtz@gmail.com>
2023-02-13 14:33:36 -0600

    adds request

--- lib/traefik.ex
+++ lib/traefik.ex
@@ -16,3 +16,17 @@ defmodule Traefik do
     :world
   end
 end
+
+IO.puts("Hola Mundo")
+
+request = """
+GET / HTTP/1.1
+Accept: */*
+Accept-Encoding: gzip, deflate
+Connection: keep-alive
+Host: google.com
+User-Agent: HTTPie/3.2.1
+
+"""
+
+IO.puts(request)
