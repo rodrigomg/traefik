861a59c391f258559b974f9fca4a099c08d83fb0
technosoul <rockdrigo.mtz@gmail.com>
2023-02-26 19:38:19 -0600

    deletes mexican debbuger

--- lib/traefik/plugs.ex
+++ lib/traefik/plugs.ex
@@ -1,18 +1,34 @@
 defmodule Traefik.Plugs do
   alias Traefik.Conn
 
+  @moduledoc """
+  Some functions for extra information about requests
+  """
+
   def rewrite_path(%Conn{path: "/redirectme"} = conn) do
     %{conn | path: "/all"}
   end
 
   def rewrite_path(%Conn{} = conn), do: conn
 
-  def log(%Conn{} = conn), do: IO.inspect(conn, label: "Logger")
+  def log(%Conn{} = conn) do
+    show_log(conn, Mix.env())
+    conn
+  end
 
-  def track(%Conn{status: 404, path: path} = conn) do
-    IO.inspect("Warn path #{path} not found")
+  @doc """
+  Shows the request when the env is active
+  """
+  def track(%Conn{status: 404, path: _path} = conn) do
+    show_track(conn, Mix.env())
     conn
   end
 
   def track(%Conn{} = conn), do: conn
+
+  defp show_log(conn, :dev), do: IO.inspect(conn)
+  defp show_log(_conn, _), do: :ok
+
+  defp show_track(_conn, :test), do: :ok
+  defp show_track(conn, _), do: IO.inspect(conn)
 end
