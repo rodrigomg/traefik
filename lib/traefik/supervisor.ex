a713934bd097746fda530a34e596d31e27c330e2
technosoul <rockdrigo.mtz@gmail.com>
2023-03-12 20:54:07 -0600

    adds simple supervisor

---
+++ lib/traefik/supervisor.ex
@@ -0,0 +1,25 @@
+defmodule Traefik.Supervisor do
+  use Supervisor
+
+  def start_link(opts) do
+    Supervisor.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  def init(_) do
+    children = [
+      Traefik.ClockServer,
+      {Traefik.NodeQueue, [name: NodeQueue]},
+      Supervisor.child_spec(
+        {Traefik.PoolQueue,
+         [
+           worker: {Traefik.FibonacciGenServer, :start_link, []},
+           n_workers: 3,
+           name: PoolFibonacci
+         ]},
+        id: :pool_queue
+      )
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
